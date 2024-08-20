1e1d7652e6521601ba7d670883a13e4d776a8688
Dom21Nguyen <dke.nguyen@gmail.com>
2024-08-20 12:00:19 -0700

    test

---
+++ PublishNugetPackageDemo/Example.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublishNugetPackageDemo
+{
+    public class Example
+    {
+        public int Add(int a, int b)
+        { return a + b; }
+    }
+}
